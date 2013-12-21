f15f295a6747355931de984b92ca3b089d4c0846
= <=>
2013-12-21 09:01:10 +0700

    New build

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5346,7 +5346,7 @@ angular.module('employeeApp').directive('tableList', [
         });
         scope.loadNext = function () {
           if (!fetching) {
-            Notification.display('Loading more upholsteries...', false);
+            Notification.display('Loading more tables...', false);
             fetching = true;
             Table.query({
               offset: scope.tables.length,
@@ -5356,6 +5356,9 @@ angular.module('employeeApp').directive('tableList', [
               Notification.hide();
               for (var i = 0; i < resources.length; i++) {
                 scope.tables.push(resources[i]);
+              }
+              if (resources.length === 0) {
+                fetching = true;
               }
             });
           }
@@ -5674,3 +5677,39 @@ angular.module('employeeApp').controller('MainCtrl', [
     }
   }
 ]);
+angular.module('employeeApp').directive('img', [function () {
+    function position(spec) {
+      console.log(spec);
+      if (spec.height && spec.parentHeight && spec.elementHeight) {
+        var heightDiff = spec.parentHeight - spec.elementHeight;
+        spec.element.css('top', heightDiff / 2);
+      } else if (spec.width && spec.parentWidth && spec.elementWidth) {
+        var widthDiff = spec.parentWidth - spec.elementWidth;
+        spec.element.css('left', widthDiff / 2);
+      }
+    }
+    return {
+      restrict: 'E',
+      compile: function prelink(tElement, tAttrs) {
+        if (!tElement.hasClass('logo')) {
+          tElement.addClass('preloaded');
+        }
+        return {
+          post: function postLink(scope, element, iAttrs) {
+            var parent = element.parent();
+            var pWidth = parent.innerWidth();
+            var pHeight = parent.innerWidth();
+            element.one('load', function () {
+              position({
+                element: element,
+                width: true,
+                parentWidth: pWidth,
+                elementWidth: element.outerWidth()
+              });
+              element.removeClass('preloaded');
+            });
+          }
+        };
+      }
+    };
+  }]);
