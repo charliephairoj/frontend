d5cb934dc6810b01ca7d8e6548f19f9c71a03ec5
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-02-12 09:41:23 -0800

    Complete most parts of the add-employee directive including: design, inputs and profile image uploader linking

--- app/scripts/controllers/dialogs/add-employee.js
+++ app/scripts/controllers/dialogs/add-employee.js
@@ -1,6 +1,54 @@
 
 angular.module('employeeApp')
-.controller('DialogsAddEmployeeCtrl', ['$scope', 'Employee', 
-function ($scope, Employee) {
-    
+.controller('DialogsAddEmployeeCtrl', ['$scope', 'Employee', '$mdToast',
+function ($scope, Employee, $mdToast) {
+	
+	$scope.employee = new Employee();
+	$scope.departments = [
+		'carpentry',
+		'painting',
+		'polishing',
+		'sewing',
+		'upholstery',
+		'foam',
+		'management',
+		'tufting',
+		'shearing',
+		'dying',
+		'inventory',
+		'packing',
+		'front office',
+		'graphics',
+		'accounting',
+		'landscaping'
+	];
+	
+	
+    $scope.addImage = function (data) {
+		
+		var image = data.hasOwnProperty('data') ? data.data : data;
+		$scope.employee.image = image;
+		
+    };
+	
+	$scope.create = function () {
+		$mdToast.show($mdToast.simple()
+			.hideDelay(0)
+			.position('top right')
+			.content('Creating employee ' + $scope.employee.first_name + '.'));
+		$scope.employee.$create(function () {
+			$mdToast.hide();
+			$mdToast.show($mdToast.simple()
+				.hideDelay(2000)
+				.position('top right')
+				.content('Employee ' + $scope.employee.first_name + ' created.'));
+				
+			$scope.reset();
+		});
+	}
+	
+	$scope.reset = function () {
+		$scope.employee = new Employee();
+	}
+	
 }]);
--- test/spec/controllers/dialogs/add-employee.js
+++ test/spec/controllers/dialogs/add-employee.js
@@ -6,16 +6,34 @@ describe('Controller: DialogsAddEmployeeCtrl', function () {
   	beforeEach(module('employeeApp'));
 
   	var DialogsAddEmployeeCtrl,
+		$http,
     	scope;
 
   	// Initialize the controller and a mock scope
-  	beforeEach(inject(function ($controller, $rootScope) {
-    
+  	beforeEach(inject(function ($controller, $rootScope, $injector) {
+    	$http = $injector.get('$httpBackend');
 		scope = $rootScope.$new();
     	DialogsAddEmployeeCtrl = $controller('DialogsAddEmployeeCtrl', {
     		$scope: scope
     	});
-  	}));
-
   	
+	}));
+	
+	describe('Post Initialization', function () {
+		
+		describe('Creating employee', function () {
+			
+			it('should make a POST call to the server', function () {
+				$http.expectPOST('/api/v1/employee/').respond({id:4, first_name:"John"});
+				
+				scope.employee.first_name = "John";
+				scope.create();
+				
+				$http.flush();
+				expect(scope.employee).toBeDefined();
+				expect(scope.id).not.toBeDefined();
+				expect(scope.first_name).not.toBeDefined();
+			});
+		});
+	});
 });
