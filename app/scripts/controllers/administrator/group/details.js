7d3a1e6ad14628fe077df264cad3b83007c8df2f
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-03-26 15:15:47 +0700

    Changes to login page

--- app/scripts/controllers/administrator/group/details.js
+++ app/scripts/controllers/administrator/group/details.js
@@ -46,9 +46,10 @@ function ($scope, Group, Permission, $routeParams, $location) {
     /*
      * Calls for updated verions of the resources
      */
-    $scope.permissionList = Permission.query({limit: 0}, function () {
+    $scope.permissionList = Permission.query({limit: 0: page_size:10000}, function () {
         merge($scope.permissionList, $scope.group.permissions);
     });
+	
     $scope.group = Group.get({'id': $routeParams.id}, function () {
         merge($scope.permissionList, $scope.group.permissions);
     });
