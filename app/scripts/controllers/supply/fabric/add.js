aa21d0890de5cc335335769fa9d238b85ef4e741
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-03-31 11:05:15 +0700

    Fix fabric add page

--- app/scripts/controllers/supply/fabric/add.js
+++ app/scripts/controllers/supply/fabric/add.js
@@ -1,7 +1,7 @@
 
 angular.module('employeeApp')
-.controller('SupplyFabricAddCtrl', ['$scope', 'Supplier', 'Fabric', '$location', 'Notification', 'ImageUploader', 
-function ($scope, Supplier, Fabric, $location, Notification, ImageUploader) {
+.controller('SupplyFabricAddCtrl', ['$scope', 'Supplier', 'Fabric', '$location', 'Notification', 'FileUploader', 
+function ($scope, Supplier, Fabric, $location, Notification, FileUploader) {
     $scope.suppliers = Supplier.query({limit:0});
     $scope.fabric = new Fabric();
     
@@ -35,24 +35,16 @@ function ($scope, Supplier, Fabric, $location, Notification, ImageUploader) {
         
         //Notify of uploading image
         Notification.display('Uploading Image...', false);
-        var fd = new FormData();
-        
-        fd.append('image', $scope.images[0]);
-        
-        //clear the form
-        $scope.addLength = null;
-        $scope.addRemark = null;
-        
-        jQuery.ajax("fabric/image", {
-			type: 'POST',
-			data: fd,
-			processData: false,
-			contentType: false,
-			success: function (responseData) {
-				Notification.display('Image Updated');
-				angular.copy(responseData, $scope.fabric);
-				$scope.$apply();
-			}
-        });
+		
+        //Notify of uploading image        
+		var promise = FileUploader.upload($scope.images[0], "/api/v1/supply/image/");
+			promise.then(function (dataObj) {
+				Notification.display('Image Uploaded');
+
+				$scope.fabric.image = dataObj.data;
+				
+		}, function (e) {
+			Notification.display("There was an error in uploading the file");
+		});
     };
 }]);
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -3243,8 +3243,8 @@ angular.module('employeeApp').controller('SupplyFabricAddCtrl', [
   'Fabric',
   '$location',
   'Notification',
-  'ImageUploader',
-  function ($scope, Supplier, Fabric, $location, Notification, ImageUploader) {
+  'FileUploader',
+  function ($scope, Supplier, Fabric, $location, Notification, FileUploader) {
     $scope.suppliers = Supplier.query({ limit: 0 });
     $scope.fabric = new Fabric();
     //Tooltips
@@ -3273,21 +3273,13 @@ angular.module('employeeApp').controller('SupplyFabricAddCtrl', [
     $scope.upload = function () {
       //Notify of uploading image
       Notification.display('Uploading Image...', false);
-      var fd = new FormData();
-      fd.append('image', $scope.images[0]);
-      //clear the form
-      $scope.addLength = null;
-      $scope.addRemark = null;
-      jQuery.ajax('fabric/image', {
-        type: 'POST',
-        data: fd,
-        processData: false,
-        contentType: false,
-        success: function (responseData) {
-          Notification.display('Image Updated');
-          angular.copy(responseData, $scope.fabric);
-          $scope.$apply();
-        }
+      //Notify of uploading image        
+      var promise = FileUploader.upload($scope.images[0], '/api/v1/supply/image/');
+      promise.then(function (dataObj) {
+        Notification.display('Image Uploaded');
+        $scope.fabric.image = dataObj.data;
+      }, function (e) {
+        Notification.display('There was an error in uploading the file');
       });
     };
   }
