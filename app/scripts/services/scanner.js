b5e95e857b33369c688ef2d180f3fa08a5b5b4d5
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-06-05 15:16:12 +0700

    Implemented identifying strings from scanner using regexp and a timer

--- app/scripts/services/scanner.js
+++ app/scripts/services/scanner.js
@@ -1,9 +1,10 @@
 
 angular.module('employeeApp.services')
-.factory('scanner', ['$location', '$rootScope', function ($location, $rootScope) {
+.factory('scanner', ['$location', '$rootScope', '$timeout', function ($location, $rootScope, $timeout) {
 		
 	var code = '',
 		codes = '',
+		unparsedStr = '',
 		standardCodes = [ 
 			[/^PO-\d+$/, '/order/purchase_order/'],
 			[/^A-\d+$/, '/order/acknowledgement/'],
@@ -11,7 +12,10 @@ angular.module('employeeApp.services')
 			[/^S-\d+$/, '/order/shipping/']
 		],
 		customCodes = [],
-		parseStandardCodes = true;
+		parseStandardCodes = true,
+		timeoutVar = 0,
+		re = /^METROLOGICK07[A-Z]\-\d+$/,
+		codeRe = /[A-Z]\-\d+$/;
 		
 		
 	var check = function (evt) {
@@ -33,7 +37,48 @@ angular.module('employeeApp.services')
 		});
     }
     
+	Scanner.prototype.stringCheck = function (evt) {
+		
+		//Cancel previously set timeout
+		$timeout.cancel(timeoutVar);
+		
+		//Get the letter
+		var key = evt.keyCode;
+		if ((96 <= key && key <= 105) || (48 <= key && key <= 90)) {
+			var letter = String.fromCharCode((96 <= key && key <= 105) ? key - 48 : key);
+		} else if (key === 189) {
+			var letter = '-';
+		}
+		
+		//Add to unparsed string
+		unparsedStr += letter;
+		
+		//Restart the time to check the string
+		timeoutVar = $timeout(function () {
+			console.log('surprise');
+			console.log(unparsedStr);
+			if (re.test(unparsedStr)) {
+				codes = codeRe.exec(unparsedStr);
+				if (codes) {
+					code = codes[0]
+					this._dispatch(code);
+				}
+				unparsedStr = '';
+				
+				
+			}
+		}.bind(this), 500, false);
+	}
+	
+	
 	Scanner.prototype._check = function (evt, customFn) {
+		
+		/*
+		 * Checks if the string matches the preset prefix and suffix 
+		 * of the scanner
+		 */
+		this.stringCheck(evt);
+		
 		/*
 		 * Checks if the character is the start code for the 
 		 * scanner. If it is the start code, then turn on the parse
