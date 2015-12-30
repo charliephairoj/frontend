16cbe109102520580fcb0e95233725671bdfc890
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-12-30 13:14:30 +0700

    Upgraded users page to be single view page with cards that can update

--- app/scripts/controllers/administrator/user/view.js
+++ app/scripts/controllers/administrator/user/view.js
@@ -1,8 +1,40 @@
 
 angular.module('employeeApp')
-.controller('AdministratorUserViewCtrl', ['$scope', 'User', function ($scope, User) {
+.controller('AdministratorUserViewCtrl', ['$scope', 'User', 'Group', 'Notification', function ($scope, User, Group, Notification) {
 
 	//Request users from the server
 	$scope.users = User.query({limit: 0});
- 
+    $scope.groups = Group.query({limit: 0});
+	
+	$scope.searchGroups = function (query) {
+		var lowercaseQuery = angular.lowercase(query);
+		var groups = [];
+		for (var i = 0; i < $scope.groups.length; i++) {
+			if (angular.lowercase($scope.groups[i].name).indexOf(lowercaseQuery) !== -1) {
+				groups.push($scope.groups[i]);
+			}
+		}
+		
+		return groups;
+	};
+	
+	$scope.save = function (user) {
+		if ($scope.currentUser.hasPermission('change_user')) {
+		
+			Notification.display('Saving user: ' + user.username + '...', false);
+			user.$update(function () {
+				Notification.display(user.username + ' saved.');
+			});
+		}
+	};
+	
+	$scope.remove = function (user, $index) {
+		if ($scope.currentUser.hasPermission('delete_user')) {
+			Notification.display('Deleting user: ' + user.username + '...', false);
+			user.$delete(function () {
+				Notification.display(user.username + ' deleted.');
+				$scope.users.splice($index, 1);
+			});
+		}
+    };
 }]);
