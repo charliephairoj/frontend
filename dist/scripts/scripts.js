76d1fa22f57c12c4a34b0b19e5c840c370b70670
Charlie Phairojmahakij <charliephairoj@gmail.com>
2016-06-10 12:07:17 +0700

    Allow purchasing agent to view system at large

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -3521,25 +3521,26 @@ function ($scope, Employee, Payroll, Attendance) {
 angular.module('employeeApp')
 .controller('MainCtrl', ['$scope', '$location', 'Acknowledgement', 'mapMarker', 'PurchaseOrder', '$rootScope', '$log',
 function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootScope, $log) {
-	
+
 	var user = $scope.currentUser;
 	var changePage = function () {
-		if (user.hasModule('supplies') && !user.hasModule('acknowledgements') && 
-		!user.hasModule('shipping')) {
+		if (user.hasModule('supplies') && !user.hasModule('acknowledgements') &&
+
+		!user.hasModule('shipping') && !user.hasModule('po')) {
 			$rootScope.inventory = true;
 			$location.path('/scanner');
 		}
-			
+
 	};
-	
+
 	if (!$scope.currentUser.ready) {
 		user.onready = changePage;
 	} else {
 		changePage();
 	}
-	
+
 	/*
-	 * Map 
+	 * Map
 	 *
 	 */
 	var latLng = {},
@@ -3547,7 +3548,7 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 	    marker,
 		markers = {'acknowledgements': [],
 				   'purchaseOrders': []},
-	    //Options for the map 
+	    //Options for the map
 		mapOptions = {
 			center: new google.maps.LatLng(13.776239, 100.527884),
 			zoom: 4,
@@ -3583,57 +3584,57 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 		    ]
 		}
 	];
-	
+
 	//Function to get zoom and position
 	function setMapFocus(latArray, lngArray){
 		var ne = new google.maps.LatLng(Math.max.apply(null, latArray), Math.max.apply(null, lngArray)),
 			sw = new google.maps.LatLng(Math.min.apply(null, latArray), Math.min.apply(null, lngArray));
-		
+
 		var bounds = new google.maps.LatLngBounds(sw, ne);
-		map.fitBounds(bounds);	
+		map.fitBounds(bounds);
 	}
-	
+
 	//Initialize map
 	map = new google.maps.Map($('#main-map')[0], mapOptions);
 	map.setOptions({styles:styles});
-	
+
 	//Get purchase orders
 	$scope.pos = PurchaseOrder.query(function () {}, function () {});
 	//Get acknowledgements and create markers on map for each order
 	$scope.acknowledgements = Acknowledgement.query(function (resp) {
-			
+
 		$scope.active = 'acknowledgements';
-		
+
 		for (var i = 0; i < 20; i++) {
 			try {
 				var address = resp[i].customer;
 				if (address.latitude && address.longitude) {
-		
+
 					marker = new google.maps.Marker({
 						position: new google.maps.LatLng(address.latitude, address.longitude),
 						map: map,
 						title: "Order #" + resp[i].id,
 						draggable: false
 					});
-				
+
 					markers[$scope.active].push(marker);
 				}
 			} catch (e) {
 				$log.error(e);
 			}
-			
+
 		}
 		map.setZoom(6);
 	});
-	
-	
+
+
 	/*
 	 * Extract relevant orders
 	 */
 	function extractOrders(dataArray, dataType) {
 		var statuses,
 			validData = [];
-		
+
 		if (dataType == 'acknowledgement') {
 			statuses = ['acknowledged', 'in production', 'deposit received'];
 		} else if (dataType == 'purchase order') {
@@ -3641,7 +3642,7 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 		} else {
 			throw Error("Invalid data type.");
 		}
-		
+
 		for (var i = 0; i < dataArray.length; i++) {
 			if (statuses.indexOf(dataArray[i].status.toLowerCase()) > -1) {
 				validData.push(dataArray[i]);
@@ -3649,18 +3650,18 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 		}
 		return validData;
 	}
-	
-	/* 
+
+	/*
  	 * Show markers for pending orders or pending purchase orders
 	 */
 	$scope.view = function (target) {
-		
+
 		var arrayHolder,
-			getAddress, 
+			getAddress,
 			markerArray,
 			lats = [],
 			lngs = [];
-		
+
 		for (var i in markers[$scope.active]) {
 			try {
 				markers[$scope.active][i].setMap(null);
@@ -3668,10 +3669,10 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 				$log.error(e);
 			}
 		}
-		
+
 		$scope.active = target;
 		markers[$scope.active] = [];
-			
+
 		if (target === 'acknowledgements') {
 			arrayHolder = extractOrders($scope.acknowledgements, 'acknowledgement');
 			getAddress = function (dataObj) {
@@ -3683,33 +3684,33 @@ function ($scope, $location, Acknowledgement, mapMarker, PurchaseOrder, $rootSco
 				return dataObj.supplier.addresses[0] || {};
 			};
 		}
-		
+
 		for (i = 0; i < arrayHolder.length; i++) {
 			try {
 				var address = getAddress(arrayHolder[i]);
 				if (address.latitude && address.longitude) {
-					
-					//Add to lats and lngs for later calculations 
+
+					//Add to lats and lngs for later calculations
 					lats.push(address.latitude);
 					lngs.push(address.longitude);
-					
+
 					var marker = new google.maps.Marker({
 						position: new google.maps.LatLng(address.latitude, address.longitude),
 						map: map,
 						title: (arrayHolder[i].customer || arrayHolder[i].supplier).name,
 						draggable: false
 					});
-					
+
 					markers[$scope.active].push(marker);
 				}
 			} catch (e) {
 				$log.error(e);
 			}
 		}
-		
+
 		setMapFocus(lats, lngs);
 	};
-	
+
 }]);
 
 
