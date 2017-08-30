2c1cdfb3bfe1f0115a15d87a9d1f1e4b9cd9f70c
Charlie Phairojmahakij <charliephairoj@gmail.com>
2017-08-30 18:40:20 +0700

    ad just purchase order

--- app/scripts/controllers/order/purchase_order/view.js
+++ app/scripts/controllers/order/purchase_order/view.js
@@ -67,6 +67,189 @@ function ($scope, PurchaseOrder, $filter, KeyboardNavigation, $location, Notific
 	};
 
 
+	/**
+	 * Shows the dialog to add a new deal
+	 * @private
+	 * @param {String|Object|Array|Boolean|Number} paramName Describe this parameter
+	 * @returns Describe what it returns
+	 * @type String|Object|Array|Boolean|Number
+	 */
+	$scope.showAcknowledgement = function (ack) {
+		
+		$mdDialog.show({
+			templateUrl: 'views/templates/view-acknowledgement.html',
+			controllerAs: 'ctrl',
+			locals: {
+				'customers': $scope.customers,
+				'acknowledgement': ack
+			},
+			controller: function ($scope, $mdDialog, customers, acknowledgement) {
+				$scope.ack = Acknowledgement.get({'id':acknowledgement.id});
+				$scope.customers = customers;
+				$scope.tempComponent = {};
+				$scope.openAttachment = function (link) {
+					window.open(link);
+				};
+				
+				/**
+				 * FILES SECTIONs
+				 *
+				 * This section deals with files that are associated or to be associated with this acknowledgement
+				 */
+
+				/**
+				 * Add files to the file uploader. On callback the files are then associated with the acknowledgement.
+				 * @public
+				 * @param {Array} files - Array of files with raw data
+				 * @returns {null}
+				 */
+				$scope.addFiles = function (files, acknowledgement) {
+					
+					$scope.ack.files = ack.files || []; 
+				
+					/* jshint ignore:start */
+					
+					Notification.display('Uploading files', 2000);
+					
+					for (var i = 0; i < files.length; i++) {
+						$scope.ack.files.push({filename: files[i].name});
+					
+						var promise = FileUploader.upload(files[i], "/api/v1/acknowledgement/file/");
+						promise.then(function (result) {
+							var data = result.data || result;
+							for (var h = 0; h < $scope.ack.files.length; h++) {
+								if (data.filename == $scope.ack.files[h].filename) {
+									angular.extend($scope.ack.files[h], data);
+								}
+							}
+							
+							Notification.display('File Uploaded', 2000);
+
+							$scope.ack.$update(function (resp) {
+								Notification.display('Acknowledgement ' + $scope.ack.id + ' saved.', 2000);
+								console.log($scope.ack);
+							}, function (e){
+								Notification.display(e, 0);
+							});
+							
+						}, function (e) {
+							$log.error(JSON.stringify(e));
+							Notification.display(e.message, 0);
+							
+						});
+					}
+				
+					/* jshint ignore:end */
+				};
+
+				/**
+				 * Add files to the file uploader. On callback the files are then associated with the acknowledgement.
+				 * @public
+				 * @param {Array} files - Array of files with raw data
+				 * @returns {null}
+				 */
+				$scope.addImage = function (files, item) {
+					
+					if (files.length > 0) {
+						/* jshint ignore:start */	
+						
+						Notification.display('Uploading image...');
+							
+						var promise = FileUploader.upload(files[0], "api/v1/acknowledgement/item/image");
+						promise.then(function (result) {
+							var data = result.data || result;
+							item.image = data
+							Notification.display('Image uploaded.');
+							
+						}, function (e) {
+							$log.error(JSON.stringify(e));
+							
+							Notification.display(e.message, 0);
+							
+						});
+						/* jshint ignore:end */
+					}
+				};
+
+
+				/**
+				 * FABRIC SECTION
+				 * 
+				 * This section deals with the product listing and search
+				 */
+				
+				// Inital list of upholsteries
+				$scope.fabricSearchText = null;
+				$scope.fabrics = Fabric.query({page_size:9999, offset:0, limit:0});
+				
+				// Watch on productSearchText to get products from the server
+				$scope.retrieveFabrics = function (query) {		
+					if (query) {
+						Fabric.query({q:query}, function (responses) {
+							for (var i = 0; i < responses.length; i++) {
+								if ($scope.fabrics.indexOfById(responses[i]) === -1) {
+									$scope.fabrics.push(responses[i]);
+								}
+							}
+						});
+					}
+				};
+				
+				/**
+				 * Returns a list of fabric whose description matches the search term
+				 *
+				 * @public
+				 * @param {String} query - Search term to apply against fabric.description
+				 * @returns {Array} - An array of fabrics whose description matches the search term
+				 */
+				$scope.searchFabrics = function (query) {
+					var lowercaseQuery = angular.lowercase(query.trim());
+					var fabrics = [];
+					for (var i = 0; i < $scope.fabrics.length; i++) {
+						try{
+							if (angular.lowercase($scope.fabrics[i].description).indexOf(lowercaseQuery) !== -1) {
+								fabrics.push($scope.fabrics[i]);
+							}
+						} catch(e) {
+							
+						}
+						
+					}
+					return fabrics;
+				};
+				
+				$scope.addComponent = function (item, component) {
+					item.components.push(angular.copy(component));
+
+					$scope.tempComponent = {};
+
+				}
+	
+				/**
+				 * Save the acknowledgement
+				 * 
+				 * @public
+				 * @param {Object} acknowledgement - The acknowledgement to be saved
+				 */
+				$scope.update = function (acknowledgement) {
+					
+					Notification.display("Updating order #" + acknowledgement.id);
+					
+					acknowledgement.$update(function () {
+						Notification.display("Order #" + acknowledgement.id + " updated");
+					});
+				};
+				
+				
+				$scope.cancel = function () {
+					$mdDialog.hide();
+				}
+			},
+			clickOutsideToClose: true
+		});
+	};
+
+
 	$scope.updateStage = function (po, status) {
 
 		if (po.status.toLowerCase() == "approved" && po.approval_pass || 1==1) {
