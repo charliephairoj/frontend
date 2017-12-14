b50f5f2e994020457d8419f36cf213b50dcf81ba
Charlie Phairojmahakij <charliephairoj@gmail.com>
2017-12-15 03:01:54 +0700

    viewing Attendances require both start and end date

--- app/scripts/controllers/hr/employee/view.js
+++ app/scripts/controllers/hr/employee/view.js
@@ -535,26 +535,27 @@ function ($scope, Employee, Notification, $mdDialog, FileUploader, $log, Shift, 
 	$scope.retrieveAttendances = function (start_date, end_date, employee) {
 		var options = {};
 		
-		if (start_date) {
+		if (start_date && end_date) {
 			options.start_date = start_date;
-		}
-		
-		if (end_date) {
 			options.end_date = end_date;
+
+			options.employee_id = employee.id;
+			
+			Attendance.query(options, function (resp) {
+				for (var i = 0; i < resp.length; i++) {
+					if (resp[i].overtime_request){
+						resp[i].overtime_request = new Date(resp[i].overtime_request);
+					}
+				}
+				employee.attendances = resp;
+				
+				
+			});
 		}
 		
-		options.employee_id = employee.id;
 		
-		Attendance.query(options, function (resp) {
-			for (var i = 0; i < resp.length; i++) {
-				if (resp[i].overtime_request){
-					resp[i].overtime_request = new Date(resp[i].overtime_request);
-				}
-			}
-			employee.attendances = resp;
-			
-			
-		});
+		
+		
 	}
 	
 	/**
