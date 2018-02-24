0ec1951770ce5e5f9800dd57f287320f3a5830c0
Charlie Phairojmahakij <charliephairoj@gmail.com>
2018-02-24 20:55:34 +0700

    updated the ui page for adding new product

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -11489,35 +11489,30 @@ angular.module('employeeApp')
 function ($scope, Model, Notification, $location, FileUploader) {
       
     $scope.model = new Model();
-  
-    //Tooltips
-    $scope.nameText = "Enter a Name for this Model";
-    $scope.modelText = "Enter a Model Number for this Model";
-    $scope.collectionText = "Enter this Model's Collection";
     
     //Adds a new models
     $scope.save = function (model) {
         Notification.display('Saving Model...', false);
-        $scope.model.$create(function () {
-            $location.path('/product/model');
+        $scope.model.$create(function (obj) {
+            $location.path('/product/model/'+obj.id);
             Notification.display('Model Saved'); 
             
         });
     };
    
-    $scope.uploadImage = function () {
+    $scope.addFiles = function (image) {
 		//Notify of uploading image
 		Notification.display('Uploading Image...', false);
 		
         //Notify of uploading image        
-		var promise = FileUploader.upload($scope.images[0], "/api/v1/upholstery/image/");
+		var promise = FileUploader.upload(image, "/api/v1/upholstery/image/");
 			promise.then(function (dataObj) {
 				Notification.display('Image Uploaded');
-
-				$scope.model.image = dataObj.data;
+                $scope.model.images = $scope.model.images || [];
+                $scope.model.images.push(dataObj.data);
 				
 		}, function (e) {
-			Notification.display("There was an error in uploading the file");
+			Notification.display("There was an error in uploading the image");
 		});
 	};
 }]);
